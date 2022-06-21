4e9afc88e32441b5d31513eb56755d117c88c4d7
Pedro Luiz <105122745+pedroooluiz@users.noreply.github.com>
2022-06-21 13:38:19 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+var inicio = document.getElementById('inicio')
+var fim = document.getElementById('fim')
+var pula = document.getElementById('pula')
+var contar = document.getElementById('contando')
+
+function verificar(){
+    if(pula.value == 0 && Number(pula.value == 0)){
+        alert('Pulando no mínimo de 1 em 1')
+        pula.value++
+    }
+    var n1 = Number(inicio.value)
+    var n2 = Number(fim.value)
+    var n3 = Number(pula.value)
+    while(n1 <= n2){
+        contar.innerHTML += `${n1} &#128073`
+        n1 += n3
+    } 
+    contar.innerHTML += ` Fim`
+}
